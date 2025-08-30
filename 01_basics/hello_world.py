a983b7332b8701a4d8d3c43188d64a4d5fb83cfb
Prem Karhale <premkarhale383@gmail.com>
2025-08-30 17:42:53 +0530

    first intro of python

--- 01_basics/hello_world.py
+++ 01_basics/hello_world.py
@@ -1,4 +1,9 @@
+# python has inbuild libraries that we can directly import into the terminal and use their methods ,also we can write the fuctionality in any file if we want to check its working, we import that file into the terminal and use that method like file_name.method() to acces the any method from the given file.
 print("hello python")
 def chai(n):
     print(n)
 chai("prem")
+
+chai_one = "masala chai"
+chai_two = "green tea " #After importing reload from importlib i got access of all the methods , variables that i added later on !!
+chai_three = "lemon tea "
