1809bc381ed5ec738d20f9d1dcf9677ef3770859
Prem Karhale <premkarhale383@gmail.com>
2025-08-31 02:02:12 +0530

    Tuple

---
+++ 01_basics/tuple.py
@@ -0,0 +1,7 @@
+tea_types =("black","white","oolong")
+print(tea_types)
+print(tea_types[0])
+print(tea_types[1:3])
+more_tea =("herbal" , "Earl grey")
+all_tea = more_tea + tea_types
+print(all_tea)
