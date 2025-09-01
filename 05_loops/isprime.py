589c8bd3a254737d94e992f9cdc75b2f886a2f6c
Prem Karhale <premkarhale383@gmail.com>
2025-09-01 14:32:37 +0530

    Is_Prime solution

---
+++ 05_loops/isprime.py
@@ -0,0 +1,10 @@
+while True:
+    number =int(input("Enter the no.:"))
+    is_prime =True
+    if number>1:
+        for i in range(2,number):
+            if (number % i)==0:
+                is_prime =False
+                break
+
+    print( is_prime ,f"{number} is prime")   
