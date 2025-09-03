1d37b9cb45783b7ea5d1965eae4dcf4109e65dc8
Prem Karhale <premkarhale383@gmail.com>
2025-09-03 13:30:41 +0530

    Sum of even no

---
+++ 05_loops/sum_of_evenno.py
@@ -0,0 +1,8 @@
+no_form_user =int(input("enter the no:"))
+sum=0;
+for i in range(no_form_user):
+    if(i%2 ==0):
+        sum=sum+i
+print("sum of the even no's form 0 to",no_form_user,"is:",sum)        
+   
+
