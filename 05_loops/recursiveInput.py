d14c2c80a9948e5fb6e6a425f4b08201701a5168
Prem Karhale <premkarhale383@gmail.com>
2025-09-03 13:30:21 +0530

    recursive_input

---
+++ 05_loops/recursiveInput.py
@@ -0,0 +1,8 @@
+while True:
+    number= int(input("Enter the no between 1 and 10 "))
+    if 1<=number <=10:
+        print("thanks")
+        break
+    else:
+        print("Invalid number \"try again\'")
+   
