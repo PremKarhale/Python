3f8c691c9d691d4aeef54e70dd54f7fd5a0cb871
Prem Karhale <premkarhale383@gmail.com>
2025-09-04 22:38:52 +0530

    Key arguments

---
+++ Function/kargs.py
@@ -0,0 +1,9 @@
+def print_kargs(**kargs):  #key arguments !!
+    for key,value in kargs.items():
+        print(f"{key},{value}")
+print_kargs(name="prem" , age =20 , branch ="IT")  #  key ,value pair 
+print_kargs(name="premkahale")       
+
+
+
+   
