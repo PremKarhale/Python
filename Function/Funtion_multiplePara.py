330dd5183e809f9bab0886c6b9e667e94ae87d7a
Prem Karhale <premkarhale383@gmail.com>
2025-09-04 22:35:34 +0530

    polymorphism

---
+++ Function/Funtion_multiplePara.py
@@ -0,0 +1,10 @@
+def sum( num1 , num2):
+    return num1 + num2
+result =sum( 3,4 )
+print(result)
+
+# function polymorphism [Many forms of the same thing ]
+def multiply (p1,p2):
+    return p1*p2
+print(multiply(3,4))
+print(multiply("prem",3))   
